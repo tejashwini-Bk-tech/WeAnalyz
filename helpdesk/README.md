6eb50c49e8b97691f40a443effd4db0f3335d259
tejashwini-Bk-tech <tejashwinikoli2005@gmail.com>
2025-11-23 23:12:58 +0530

    "intialize commit"

---
+++ helpdesk/README.md
@@ -0,0 +1,1 @@
+<h2>Building the helpdisk-UI</h2>
---
+++ helpdesk/src/components/layouts/sidebar.tsx
@@ -0,0 +1,9 @@
+import { Home,Ticket,Plus } from "lucide-react";
+import { NavLink } from "react-router-dom";
+
+export default function Sidebar(){
+    return(
+        <>
+        </>
+    )
+}
